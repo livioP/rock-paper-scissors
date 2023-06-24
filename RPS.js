e7c1f2f174b59a41a44d320e5d573f72ba1e321e
livioP <137178163+livioP@users.noreply.github.com>
2023-06-24 16:43:55 +0200

    edit 24/06

--- RPS.js
+++ RPS.js
@@ -1,38 +1,59 @@
-let button1 = document.querySelector('#btn1');
-let button2 = document.getElementById('btn2');
-let button3 = document.getElementById('btn3');
+let button1 = document.getElementById("btn1");
+let button2 = document.getElementById("btn2");
+let button3 = document.getElementById("btn3");
+let bottoni = document.querySelectorAll("button");
+let playerText = document.getElementById("playerText")
+let computerText = document.getElementById("computerText")
+let result = document.getElementById("result")
+let playerChoice
+let compChoice
 
 
-const x = Math.floor(Math.random() * 3);
 
-function getComputerChoice(x){
+
+
+/////////////////////// Tasti Player
+
+bottoni.forEach(bottoni => bottoni.addEventListener("click", () => { 
+    playerChoice = bottoni.value;
+    getComputerChoice();
+    playerText.innerHTML = `Player choice:  ${playerChoice}`;
+    computerText.innerHTML = `Computer choice:  ${compChoice}`;
+    result.innerHTML = `${playRound(playerChoice, compChoice)}`;
+}))
+
+///////////////////////////////////
+
+function getComputerChoice(){
+    const x = Math.floor(Math.random() * 3);
         if (x === 1){
-            return 'Rock'
+            return compChoice = 'Rock'
         } else if (x === 2){
-            return 'Paper'
-        } else return 'Scissors'
+            return compChoice = 'Paper'
+        } else 
+            return compChoice = 'Scissors'
 };
 
-const compChoice = getComputerChoice(x);
-console.log(compChoice);
 
 
-function btn1(){
-button1.addEventListener('click', () => {
-    return 'Rock';
-});
+function playRound(playerChoice, compChoice){
+    if (playerChoice === 'Rock' && compChoice === 'Rock'){
+        return "It's a tie!"
+    } else if (playerChoice === 'Rock' && compChoice === 'Paper'){
+        return "Oh no, computer wins"
+    } else if (playerChoice === 'Rock' && compChoice === 'Scissors'){
+        return 'You WIN!'
+    } else if (playerChoice === 'Paper' && compChoice === 'Paper'){
+        return "It's a tie!"
+    } else if (playerChoice === 'Paper' && compChoice === 'Scissors'){
+        return "Oh no, computer wins"
+    } else if (playerChoice === 'Paper' && compChoice === 'Rock'){
+        return 'You WIN!'
+    } else if (playerChoice === 'Scissors' && compChoice === 'Scissors'){
+        return "It's a tie!"
+    } else if (playerChoice === 'Scissors' && compChoice === 'Rock'){
+        return "Oh no, computer wins"
+    } else if (playerChoice == 'Scissors' && compChoice == 'Paper'){
+    return 'You WIN!'
+    }
 }
-
-button2.addEventListener('click', () => {
-    return 'Paper';
-});
-button3.addEventListener('click', () => {
-    return 'Scissors';
-});
-
-
-function myFunction(btn) {
-    const playerChoice = btn.value
-}
-
-console.log(playerChoice)
