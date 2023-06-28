3a293108d70ba136aac0ef00b1e5ac6b4366f34c
livioP <137178163+livioP@users.noreply.github.com>
2023-06-28 12:46:11 +0200

    works

--- RPS.js
+++ RPS.js
@@ -8,34 +8,42 @@ let result = document.getElementById("result")
 let score = document.getElementById("punteggio");
 let playerChoice
 let compChoice
-let playerScore = 0
-let computerScore = 0
-
-
-
+let playerScore = 0;
+let computerScore = 0;
+let risposta;
 
 /////////////////////// 
-function round() {
+
+
+
+
+
+
+function round(){
     bottoni.forEach(bottoni => bottoni.addEventListener("click", () => { 
-        playerChoice = bottoni.value;
-        getComputerChoice();
-        playerText.innerHTML = `Player choice:  ${playerChoice}`;
-        computerText.innerHTML = `Computer choice:  ${compChoice}`;
-        score.innerText = `Player: ${playerScore} - Computer: ${computerScore}`;
-        result.innerHTML = `${playRound(playerChoice, compChoice)}`;
-            if (result.innerHTML === "Oh no, computer wins"){
-                 return computerScore++
-            } else if (result.innerHTML === 'You WIN!'){
-                return playerScore++;
-            } else console.log('tie')
-                  
+    playerChoice = bottoni.value;
+    getComputerChoice();
+    playRound(playerChoice, compChoice);
+    score.innerText = `player: ${playerScore}, computer: ${computerScore}`
+        if (risposta == 'computer wins'){
+            result.innerText = 'Point for PC :('
+        } else if (risposta == 'you win'){
+            result.innerText = 'Point for you!!!'
+        } else if (risposta == `It's a tie!`)
+        result.innerText = `It's a tie!`;
+    playerText.innerText = `Player choice: ${playerChoice}`;
+    computerText.innerText = `Player choice: ${compChoice}`;    
+
 }))
 }
 
+function game(){
+    if (playerScore < 5 && computerScore < 5){
+        round();
+    }
+}
 
-round()
-
-
+game()
 ///////////////////////////////////
 
 function getComputerChoice(){
@@ -51,22 +59,30 @@ function getComputerChoice(){
 
 function playRound(playerChoice, compChoice){
     if (playerChoice === 'Rock' && compChoice === 'Rock'){
-        return "It's a tie!"
+        return risposta = "It's a tie!"
+    
     } else if (playerChoice === 'Rock' && compChoice === 'Paper'){
-        return "Oh no, computer wins"
+        return computerScore++, risposta = 'computer wins';
+    
     } else if (playerChoice === 'Rock' && compChoice === 'Scissors'){
-        return 'You WIN!'
+        return playerScore++, risposta = 'you win';
+    
     } else if (playerChoice === 'Paper' && compChoice === 'Paper'){
-        return "It's a tie!"
+        return risposta = "It's a tie!"
+    
     } else if (playerChoice === 'Paper' && compChoice === 'Scissors'){
-        return "Oh no, computer wins"
+        return computerScore++, risposta = 'computer wins';
+    
     } else if (playerChoice === 'Paper' && compChoice === 'Rock'){
-        return 'You WIN!'
+        return playerScore++, risposta = 'you win';
+    
     } else if (playerChoice === 'Scissors' && compChoice === 'Scissors'){
-        return "It's a tie!"
+        return risposta = "It's a tie!"
+    
     } else if (playerChoice === 'Scissors' && compChoice === 'Rock'){
-        return "Oh no, computer wins"
+        return computerScore++, risposta = 'computer wins';
+    
     } else if (playerChoice == 'Scissors' && compChoice == 'Paper'){
-    return 'You WIN!'
+    return playerScore++, risposta = 'you win';
     }
 }
